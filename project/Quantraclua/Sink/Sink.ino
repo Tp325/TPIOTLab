339cabe9a2db7a3d2cd8d9ae2b25b2ed5271ee4e
Tp3251 <quocthinhtk@gmail.com>
2025-05-05 16:02:54 +0700

    V1.3

--- project/Quantraclua/Sink/Sink.ino
+++ project/Quantraclua/Sink/Sink.ino
@@ -6,10 +6,8 @@
 #include <PubSubClient.h>
 #include <SPI.h>
 #include <LoRa.h>
-
-
 // Thông tin MQTT Broker
-const char* mqtt_server = "103.221.220.183";
+const char* mqtt_server = "dev.iotlab.net.vn";
 const int mqtt_port = 1883;
 const char* mqtt_user = "api1@Iotlab";
 const char* mqtt_pass = "Iotlab@2023";
@@ -20,6 +18,8 @@ const char* mqtt_pass = "Iotlab@2023";
 #define RST 13
 #define DIO0 2
 
+bool isWifiConnect = 0;
+long int timeOutReconnectWiFi = 0;
 // Tạo client WiFi và MQTT
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
@@ -27,13 +27,13 @@ PubSubClient mqttClient(espClient);
 void setup() {
   Serial.begin(9600);
   // Khởi động LoRa
-  LoRa.setPins(SS, RST, DIO0);
-  if (!LoRa.begin(433E6)) {  // Tần số 433 MHz
-    Serial.println("Khởi động LoRa thất bại!");
-    while (1)
-      ;
-  }
-  Serial.println("LoRa đã sẵn sàng để nhận dữ liệu!");
+  // LoRa.setPins(SS, RST, DIO0);
+  // if (!LoRa.begin(433E6)) {  // Tần số 433 MHz
+  //   Serial.println("Khởi động LoRa thất bại!");
+  //   while (1)
+  //     ;
+  // }
+  // Serial.println("LoRa đã sẵn sàng để nhận dữ liệu!");
 
   // Kết nối WiFi
   setupWiFi();
@@ -43,37 +43,41 @@ void setup() {
 }
 
 void loop() {
-  if (!mqttClient.connected()) {
-    connectMQTT();
-  }
-  mqttClient.loop();
-
-  // Kiểm tra xem có gói dữ liệu nào từ LoRa
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    String receivedData = "";
-    while (LoRa.available()) {
-      receivedData += (char)LoRa.read();
+  reconnectWifi();
+  if (isWifiConnect == 1) {
+    if (!mqttClient.connected()) {
+      connectMQTT();
     }
+    mqttClient.loop();
+    // Kiểm tra xem có gói dữ liệu nào từ LoRa
+    int packetSize = LoRa.parsePacket();
+    if (packetSize) {
+      String receivedData = "";
+      while (LoRa.available()) {
+        receivedData += (char)LoRa.read();
+      }
 
-    Serial.print("Dữ liệu nhận được từ LoRa: ");
-    Serial.println(receivedData);
+      Serial.print("Dữ liệu nhận được từ LoRa: ");
+      Serial.println(receivedData);
 
-    // Gửi dữ liệu lên MQTT
-    if (mqttClient.publish("Soctrangricefield", receivedData.c_str())) {
-      Serial.println("Dữ liệu đã được gửi lên MQTT thành công!");
-    } else {
-      Serial.println("Gửi dữ liệu lên MQTT thất bại!");
+      // Gửi dữ liệu lên MQTT
+      if (mqttClient.publish("ThoaiSonCommon", receivedData.c_str())) {
+        Serial.println("Dữ liệu đã được gửi lên MQTT thành công!");
+      } else {
+        Serial.println("Gửi dữ liệu lên MQTT thất bại!");
+      }
     }
   }
 }
 
 // Hàm kết nối Wi-Fi
 void setupWiFi() {
+  WiFi.onEvent(onWiFiEvent);
+  WiFi.mode(WIFI_STA);
   Serial.print("Đang kết nối với WiFi...");
   WiFiManager wm;
   bool res;
-  res = wm.autoConnect("AutoConnectAP", "AutoConnectAP");
+  res = wm.autoConnect("QuanTracLua");
   if (!res) {
     Serial.println("Failed to connect");
     // ESP.restart();
@@ -94,5 +98,26 @@ void connectMQTT() {
       Serial.print(mqttClient.state());
       delay(2000);
     }
+  }
+}
+void reconnectWifi() {
+  if (millis() - timeOutReconnectWiFi > 20000 && isWifiConnect == 0) {
+    timeOutReconnectWiFi = millis();
+      WiFi.disconnect();
+      WiFi.reconnect();
+  }
+}
+void onWiFiEvent(WiFiEvent_t event) {
+  switch (event) {
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+      Serial.println("[WiFiEvent] Connected to WiFi!");
+      isWifiConnect = 1;
+      break;
+    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+      Serial.println("[WiFiEvent] WiFi lost, reconnecting...");
+      isWifiConnect = 0;
+      break;
+    default:
+      break;
   }
 }
